d087dbcff12bd9b5a112f9768a1f9bf022ef453c
msn560 <60465352+msn560@users.noreply.github.com>
2023-01-13 23:04:58 +0300

    Create cv2 image to pyqt5.py

---
+++ cv2 image to pyqt5.py
@@ -0,0 +1,12 @@
+    def viewCam(self):
+        # read image in BGR format
+        ret, image = self.cap.read()
+        # convert image to RGB format
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # get image infos
+        height, width, channel = image.shape
+        step = channel * width
+        # create QImage from image
+        qImg = QImage(image.data, width, height, step, QImage.Format_RGB888)
+        # show image in img_label
+        self.ui.image_label.setPixmap(QPixmap.fromImage(qImg))
